87a7e003b9712808021ce9b7a97ed291d024ad2e
cleb-collab <CLeberr@nycstudents.net>
2023-10-05 12:38:51 -0400

    proto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+public GameObject projectilePrefab;
 public float xRange = 10;
     // Update is called once per frame
     void Update()
@@ -19,6 +20,11 @@ public float xRange = 10;
 
         if (transform.position.x > xRange) {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        { 
+            //launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
