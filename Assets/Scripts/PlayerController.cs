c08dd500b93f14402673a508a3dccd44f25e1ae4
Game 3 <game3@AH4TGF00J1H8Y.local>
2023-10-31 12:41:07 -0400

    snackAttack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     public float xRange = 10;
     private float topBound = 30;
     private float lowerBound = -10;
-    float horizontalInput;
+    private float horizontalInput;
+    private float forwardInput;
     float speed = 10.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,25 +23,12 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
-
-        if (transform.position.x < -xRange) {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
-
-        if (transform.position.x > xRange) {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        { 
-            //launch a projectile from the player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
-        if (transform.position.z > topBound)
-        {
-            Destroy(gameObject);
-        } else if (transform.position.z < lowerBound){
-            Destroy(gameObject);
-        }
+      
+   
+    
     }
 }
